d7d15ffed7fc04cd9eed62c610ca0ff67364b8a9
unknown <raysdigitalservice@gmail.com>
2022-07-02 01:21:54 -0400

    env

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="text-primary-content">
+      <h1 className="text-6xl mb-4 ">Github Finder</h1>
+      <p className="text-lg text-white-400">
+        A React app to search Github profiles and see profile details.
+      </p>
+    </div>
+  )
+}
+
+export default About
