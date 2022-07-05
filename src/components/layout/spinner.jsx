fb153b1ed7c894deac22e81a5a72daeb63058748
unknown <raysdigitalservice@gmail.com>
2022-07-05 16:12:09 -0400

    context

--- src/components/layout/spinner.jsx
+++ src/components/layout/spinner.jsx
@@ -1,11 +1,16 @@
-import Spinner from './assets/spinner.gif';
+import spinner from './assets/spinner.gif';
 
-function spinner() {
+function Spinner() {
   return (
     <div className='w-100 mt-20'>'
-      <img width="180" className='text-center mx-auto' src={spinner} alt="Loading..." />
+      <img 
+        width="180" 
+        className='text-center mx-auto' 
+        src={spinner} 
+        alt="Loading..."
+      />
     </div>
   )
 }
 
-export default spinner
+export default Spinner
